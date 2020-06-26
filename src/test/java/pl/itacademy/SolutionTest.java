e04100027ba564fa530630df2edf150fa1b4d90a
olegmiercoles <oleg.miercoles@dot.com>
2020-06-26 21:52:11 +0200

    - add more test cases; - use AssertJ; - use JUnit4.

--- src/test/java/pl/itacademy/SolutionTest.java
+++ src/test/java/pl/itacademy/SolutionTest.java
@@ -1,24 +1,34 @@
 package pl.itacademy;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.List;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.equalTo;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
-class SolutionTest {
-    private Solution solution = new Solution();
+public class SolutionTest {
+    private Solution solution;
+
+    @Before
+    public void setUp() {
+        solution = new Solution();
+    }
 
     @Test
     public void solutionShouldReturnSecondMaxNumber() {
-        assertThat(solution.secondMaxNumber(List.of(5, 3, 2, 9, 1, 4, 15)), equalTo(9));
+        assertThat(solution.secondMaxNumber(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11))).isEqualTo(10);
+        assertThat(solution.secondMaxNumber(List.of(10, 9, 8, 7, 6, 5, 4, 3, 2, 1))).isEqualTo(9);
+        assertThat(solution.secondMaxNumber(List.of(-10, -9, -8, -7, -6, -5, -4))).isEqualTo(-5);
+        assertThat(solution.secondMaxNumber(List.of(-3, -4, -5, -6, -7, -8, -9, -10))).isEqualTo(-4);
+        assertThat(solution.secondMaxNumber(List.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7))).isEqualTo(6);
+        assertThat(solution.secondMaxNumber(List.of(12, 12, 11, 11, 10, 10, 9, 9, 8, 8, 7, 7, 6, 6, 5, 5))).isEqualTo(11);
+        assertThat(solution.secondMaxNumber(List.of(1, 10, 2, 9, 3, 8, 4, 7, 5, 6))).isEqualTo(9);
     }
 
     @Test
     public void solutionShouldReturnPalindromes() {
-        assertThat(solution.findPalindromes(List.of("flight", "car", "civic", "radar", "level", "day", "round", "rotor", "kayak")),
-                containsInAnyOrder("civic", "radar", "level", "rotor", "kayak"));
+        assertThat(solution.findPalindromes(List.of("flight", "car", "civic", "radar", "level", "day", "round", "rotor", "kayak")))
+                .containsExactlyInAnyOrder("civic", "radar", "level", "rotor", "kayak");
     }
 }
